5b81723fbadd1b959ac1c196f4e7b3847b29993c
janschulte <j.schulte@52north.org>
2021-02-18 16:40:26 +0000

    Deploying to gh-pages from master @ e5e1ec44a4876e9b956a24c7e74c28111c07d1b9 🚀

---
+++ room-labels.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="room-labels" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="room-labels.png" width="136" height="165"/>
+</tileset>
